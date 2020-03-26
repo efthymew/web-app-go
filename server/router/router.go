a250af98130cc597bbafe20e806c6e55ea085ac0
Graham Efthymiou <grahomie@gmail.com>
2020-03-26 05:06:28 -0400

    added a simple, pretty custom 404 page redirect

--- server/router/router.go
+++ server/router/router.go
@@ -2,6 +2,8 @@ package router
 
 import (
 	"net/http"
+	"os"
+	"path"
 
 	"github.com/gorilla/mux"
 )
@@ -10,8 +12,26 @@ import (
 func Router() *mux.Router {
 	r := mux.NewRouter()
 	//serve static files
-	r.PathPrefix("/").Handler(http.StripPrefix("/", http.FileServer(http.Dir("./frontend/build"))))
+	r.PathPrefix("/").Handler(http.StripPrefix("/", CustomFileServer(http.Dir("./frontend/build"))))
 	//r.PathPrefix("/").HandlerFunc(handlers.Index)
 	//http.Handle("/", r)
 	return r
+}
+
+//CustomFileServer c
+func CustomFileServer(fs http.FileSystem) http.Handler {
+	fsh := http.FileServer(fs)
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		_, err := fs.Open(path.Clean(r.URL.Path))
+		if os.IsNotExist(err) {
+			NotFound(w, r)
+			return
+		}
+		fsh.ServeHTTP(w, r)
+	})
+}
+
+//NotFound n
+func NotFound(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "./frontend/build/error404.html")
 }
